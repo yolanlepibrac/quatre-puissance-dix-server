d2b2b239c0b2ebcc19ee2bfb8ab7bfa45feff513
yolan <yolan.pibrac@gmail.com>
2020-02-23 17:38:53 +0100

    e

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { GamesModule } from './mongoose/game/games.module';
       'mongodb://yolanpibrac:Lasvegasparano1@ds217548.mlab.com:17548/heroku_wwwcrrdp',
     ),
     */
-    UsersModule,
+    //UsersModule,
     GamesModule,
   ],
   controllers: [AppController],
