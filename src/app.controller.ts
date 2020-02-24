fe0e22d2d68e489db02294aa70a9e0e097b6624b
yolan <yolan.pibrac@gmail.com>
2020-02-24 23:47:39 +0100

    e

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -15,24 +15,21 @@ import { UserService } from './mongoose/user/users.service';
 import { GamesService } from './mongoose/game/games.service';
 //import { Request } from 'express';
 import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
 
 @Controller('app')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly authService: AuthService) {}
 
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
-    return req.user;
+    return this.authService.login(req.user);
   }
 
   @Post()
   create(): string {
     return 'This action adds a new cat';
-  }
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
   }
 }
