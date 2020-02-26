2738d857313e0495865b9e85d24a02697e31d0b7
yolan <yolan.pibrac@gmail.com>
2020-02-26 20:46:25 +0100

    e

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,8 +9,8 @@ export class AuthService {
     private readonly jwtService: JwtService,
   ) {}
 
-  async validateUser(username: string, pass: string): Promise<any> {
-    const user = await this.usersService.findOne(username);
+  async validateUser(email: string, pass: string): Promise<any> {
+    const user = await this.usersService.findOne(email);
     if (user && user.password === pass) {
       const { password, ...result } = user;
       return result;
