91eeeb6826cb23b3cdf7106ba0d54994b9808a17
Yolan Pibrac <yolan.pibrac@gmail.com>
2022-10-15 16:35:49 +0200

    clean the app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './mongoose/user/user.module';
 import { GamesModule } from './mongoose/game/games.module';
 import { ChatGateway } from './chat/chat.gateway';
 
 @Module({
-  imports: [UserModule, GamesModule],
-  controllers: [AppController],
-  providers: [AppService, ChatGateway],
+  imports: [UserModule, GamesModule] ,
+  providers: [ ChatGateway],
 })
 export class AppModule {}
--- src/mongoose/database.providers.ts
+++ src/mongoose/database.providers.ts
@@ -5,7 +5,7 @@ export const databaseProviders = [
     provide: 'DATABASE_CONNECTION',
     useFactory: (): Promise<typeof mongoose> =>
       mongoose.connect(
-        'mongodb://yolanpibrac:Lasvegasparano1@ds217548.mlab.com:17548/heroku_wwwcrrdp',
+        process.env.DATABASE_PATH,
       ),
   },
 ];
